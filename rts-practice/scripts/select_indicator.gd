bd1005af3fe1038c8be7f66ac1797a69079b35df
DenisLjubojevic <denisljubojevic@gmail.com>
2026-05-16 22:20:46 +0200

    added new terrain and fixed navmesh

--- rts-practice/scripts/select_indicator.gd
+++ rts-practice/scripts/select_indicator.gd
@@ -2,8 +2,9 @@ extends CharacterBody3D
 
 const MODULE_CAMERA: GDScript = preload("res://scripts/moduleCamera.gd")
 
-var steerSpeed: float = 1.0
-var navigationPathGoalPosition: Vector3 
+var steerSpeed: float = 4.0
+var navigationPathGoalPosition: Vector3
+var rotationFast: bool = true
 
 @onready var selection_circle: MeshInstance3D = $SelectionCircle
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
@@ -33,22 +34,43 @@ func _input(event: InputEvent) -> void:
 
 # updates velocity of agent so he knows where to move
 func _physics_process(delta: float) -> void:
-	if navigation_agent.is_navigation_finished(): 
+	if navigation_agent.is_navigation_finished():
+		if !is_on_floor():
+			velocity.y -= 9.8 * delta
+			move_and_slide()
 		animPlayer.play("Idle")
 		return
 	
 	animPlayer.play("walk")
 	var nextPosition: Vector3 = navigation_agent.get_next_path_position()
 	var direction: Vector3 = global_position.direction_to(nextPosition) * navigation_agent.max_speed
+	
+	rotateToDirection(direction, delta)
+	
 	var steeredVelocity: Vector3 = (direction - velocity) * delta * steerSpeed
 	var newAgentVelocity: Vector3 = velocity + steeredVelocity
 	navigation_agent.set_velocity(newAgentVelocity)
 
+# make a rotation for character before starting to move
+func rotateToDirection(dir: Vector3, delta: float) -> void:
+	if rotationFast:
+		rotation.y = atan2(-dir.x, -dir.z) + deg_to_rad(90)
+	else:
+		var targetAngle: float = atan2(-dir.x, -dir.z) + deg_to_rad(90)
+		var angleDiff: float = wrapf(targetAngle - rotation.y, -PI, PI)
+		rotation.y += angleDiff * delta * steerSpeed
+
 # method to move character after it got a signal
 func charaterMove(newVelocity: Vector3) -> void:
+	var currentVelocity: float = velocity.y
 	velocity = newVelocity
+	
+	if !is_on_floor():
+		velocity.y = currentVelocity - 9.8 * get_physics_process_delta_time()
+	else:
+		velocity.y = 0.0
+	
 	move_and_slide()
-	rotation.y = atan2(velocity.x, velocity.z) + deg_to_rad(-90)
 
 # updates navigation path of characters after timer run out
 func navigationPathTimerUpdate() -> void:
